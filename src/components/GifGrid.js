e9af2c46807babc87f3d782fc71b4f06e17034a6
David <davidsantander9@gmail.com>
2021-07-05 23:18:54 -0500

    Add mock function return <GifGrid/>

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 import useFetchGifs from '../hooks/useFetchGifs';
 import GifGridItem from './GifGridItem';
@@ -26,5 +27,11 @@ const GifGrid = ({ category }) => {
         </>
     )
 }
+
+
+GifGrid.propTypes = {
+    category: PropTypes.string.isRequired,
+};
+
 
 export default GifGrid;
---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
+import GifGrid from '../../components/GifGrid';
+import useFetchGifs from '../../hooks/useFetchGifs';
+jest.mock('../../hooks/useFetchGifs');
+
+describe('<GifGrid/>  test', () => {
+
+    const category = 'Batman';
+
+    test('should correctly display the component <GifGrid/>', () => {
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true 
+        });
+        const wrapper = shallow(<GifGrid category={ category } />);
+        expect( wrapper ).toMatchSnapshot();
+    });
+
+    test('should show when loads images', () => {
+        const gifs = [{
+            id: 'avas',
+            url: 'https://localhost/cosa.png',
+            title: 'A thing'
+        }] 
+        useFetchGifs.mockReturnValue({
+            data: gifs,
+            loading: true 
+        });
+        const wrapper = shallow(<GifGrid category={ category } />);
+        expect( wrapper ).toMatchSnapshot();
+    });
+    
+    
+});

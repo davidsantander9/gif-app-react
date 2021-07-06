135c6a80027a469d7a2b450436b9368c865805da
David <davidsantander9@gmail.com>
2021-07-05 21:16:36 -0500

    Add title test in GifGridItem component

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -6,11 +6,16 @@ describe('<GifGridItem/>  test', () => {
 
     const title = 'Title';
     const url = 'http//localhost/algo.jpg';
+    const wrapper = shallow(<GifGridItem title={ title } url={ url } />);
 
     test('should correctly display the component', () => {
-        const wrapper = shallow(<GifGridItem title={ title } url={ url } />);
         expect( wrapper ).toMatchSnapshot();
     });
+    
+    test('should have a paragraph with the title', () => {
+        const p =  wrapper.find('p');
+        expect( p.text().trim() ).toBe( title );
+    })
     
 
 

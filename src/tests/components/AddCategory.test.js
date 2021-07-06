b28f2ebc6f48cc308bc405fca05c19d716369ddb
David <davidsantander9@gmail.com>
2021-07-05 23:01:31 -0500

    Simulate a function and counts how many times have benn call AddCategory

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import '@testing-library/jest-dom';
 import { shallow } from 'enzyme';
 import AddCategory from '../../components/AddCategory';
 
 describe('<AddCategory/>  test', () => {
 
-    const setCategories = () => {};
-    const wrapper = shallow(<AddCategory setCategories={ setCategories } />);
+    const setCategories = jest.fn();
+    let wrapper = shallow(<AddCategory setCategories={ setCategories } />);
+    
+    beforeEach(()=>{
+        jest.clearAllMocks();
+        wrapper = shallow(<AddCategory setCategories={ setCategories } />);
+    });
 
     test('should correctly display the component', () => {
         expect( wrapper ).toMatchSnapshot();
@@ -16,7 +22,22 @@ describe('<AddCategory/>  test', () => {
         const value = 'Hola mundo'; 
         input.simulate('change', { target:{ value } });
         expect(  wrapper.find('p').text().trim()  ).toBe(value);
-    })
+    });
+    
+    test('should not post the info', () => {
+        wrapper.find('form').simulate('submit', { preventDefault: () => {} });
+
+        expect( setCategories ).not.toHaveBeenCalled();
+    });
+
+    test('should call function setCategories clear the value', () => {
+        const input = wrapper.find('input');
+        const value = 'Hola mundo'; 
+        input.simulate('change', { target:{ value } });
+        wrapper.find('form').simulate('submit', { preventDefault: () => {} });
+        expect(  wrapper.find('p').text().trim()  ).toBe('');
+        expect( setCategories ).toHaveBeenCalled();
+    });
     
     
-});
+}); 

c2a37206c092f45f59fee93c1700d44b85dbf91d
David <davidsantander9@gmail.com>
2021-07-05 21:25:11 -0500

    Add include className test in GifGridItem component

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -15,7 +15,23 @@ describe('<GifGridItem/>  test', () => {
     test('should have a paragraph with the title', () => {
         const p =  wrapper.find('p');
         expect( p.text().trim() ).toBe( title );
+    });
+
+    test('should have the image with url and alt same as props', () => {
+        const img = wrapper.find('img');
+        expect( img.prop('src') ).toBe( url );
+        expect( img.prop('alt') ).toBe( title );
+
+    });
+
+    test('should have animate__fadeIn class', () => {
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+        expect( className.includes('animate__fadeIn') ).toBe(true);
     })
+    
+    
+
     
 
 
